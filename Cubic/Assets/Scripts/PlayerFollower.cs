e30e25d251d28a59e5871dd1fa3d675bb05bae07
Karol Kostyra <karol.m.kostyra@gmail.com>
2019-06-23 16:41:43 +0200

    Upgraded
    -Added camera to follow player as a script called 'PlayerFollower'

---
+++ Cubic/Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public bool LookAtPlayer = false;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    private Vector3 _cameraOffset;
+
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // LateUpdate is called after Update methods
+    void Update()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer)
+            transform.LookAt(PlayerTransform);
+    }
+}
